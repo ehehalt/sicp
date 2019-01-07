163cb5410c19c872608bf1a6173dd461b3ce2be8
Mike <mike42@hotmail.de>
2019-01-07 21:20:57 +0100

    Factorial

--- 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.el
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.el
@@ -5,3 +5,15 @@
   (if (= n 1)
       1
     (* n (fact1 (- n 1)))))
+
+;; Iterative
+
+(defun fact2 (n)
+  (fact2-iter 1 1 n))
+
+(defun fact2-iter (product counter max-count)
+  (if (> counter max-count)
+      product
+    (fact2-iter (* counter product)
+                (+ counter 1)
+                max-count)))
