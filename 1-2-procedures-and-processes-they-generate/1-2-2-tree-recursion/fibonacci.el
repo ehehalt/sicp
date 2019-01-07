aa1db34a69a3e9f898ee45ba10d6f2d34fa2f520
Mike <mike42@hotmail.de>
2019-01-07 20:11:04 +0100

    Fibonacci iterativ

--- 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.el
+++ 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.el
@@ -1,8 +1,17 @@
 ;; SICP 1.2.2
 ;; Tree Recursion
- 
-(defun fib (n)
+
+(defun fib1 (n)
   (cond ((= n 0) 0)
         ((= n 1) 1)
         (t (+ (fib (- n 1))
               (fib (- n 2))))))
+
+
+(defun fib2 (n)
+	(fib2-iter 1 0 n))
+
+(defun fib2-iter (a b count)
+	(if (= count 0)
+			b
+		(fib2-iter (+ a b) a (- count 1))))
