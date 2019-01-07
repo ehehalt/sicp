6cd5e1c7abc123e1b40d4a5a5992d3c56d4530ce
Mike <mike42@hotmail.de>
2019-01-07 20:05:50 +0100

    Untabified

--- 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.el
+++ 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.el
@@ -2,7 +2,7 @@
 ;; Tree Recursion
  
 (defun fib (n)
-	(cond ((= n 0) 0)
-				((= n 1) 1)
-				(t (+ (fib (- n 1))
-							(fib (- n 2))))))
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (t (+ (fib (- n 1))
+              (fib (- n 2))))))
