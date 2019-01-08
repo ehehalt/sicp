438a877b89d70c895fd3f4ba19194404e4c8cf63
Mike <mike42@hotmail.de>
2019-01-08 22:55:43 +0100

    Haskel factorial

---
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.hs
@@ -0,0 +1,18 @@
+-- SICP 1.2.1
+-- Linear Recursion and Iteration
+
+fact1 :: Int -> Int
+fact1 1 = 1
+fact1 n = n * fact1(n - 1)
+
+
+-- Iterative
+
+fact2 :: Int -> Int
+fact2 n = fact2iter 1 1 n
+
+fact2iter :: Int -> Int -> Int -> Int
+fact2iter product counter maxcount
+  | counter > maxcount = product
+  | otherwise = fact2iter (counter*product) (counter+1) maxcount
+
