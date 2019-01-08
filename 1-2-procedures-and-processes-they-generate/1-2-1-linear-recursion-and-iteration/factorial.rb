544478c18ec2fec8dd956bd69135baf2b38c99fd
Mike <mike42@hotmail.de>
2019-01-08 23:02:06 +0100

    Ruby fixed

--- 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.rb
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 
 # SICP 1.2.1
-# Linear Recursion and Interation# Iterative
+# Linear Recursion and Interation
+
+def fact1(n)
+  return 1 if n == 1
+  return n * fact1(n - 1)
+end
+
+# Iterative
 
 def fact2(n)
   return fact2iter(1, 1, n)
