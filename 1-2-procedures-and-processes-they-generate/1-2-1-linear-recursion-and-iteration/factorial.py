0f8ce573665311b8d4372be7429b7ffac999e508
Mike <mike42@hotmail.de>
2019-01-08 22:42:47 +0100

    Factorial in Python

---
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# SICP 1.2.1
+# Linear Recursion and Iteration
+
+def fact1(n: int) -> int:
+    if n == 1:
+        return 1
+    else:
+        return n * fact1(n - 1)
+
+# Iterative
+
+def fact2(n: int) -> int:
+    return fact2Iter(1, 1, n)
+
+def fact2Iter(product, counter, maxCount: int) -> int:
+    if counter > maxCount:
+        return product
+    else:
+        return fact2Iter(counter*product, counter+1, maxCount)
+
