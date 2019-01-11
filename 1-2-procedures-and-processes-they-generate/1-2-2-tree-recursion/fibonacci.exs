b8c8573d6249f777c050ce75469d2d7f66409c00
Mike <mike42@hotmail.de>
2019-01-12 00:10:50 +0100

    Tabs2Spaces

--- 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.exs
+++ 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.exs
@@ -2,14 +2,15 @@
 # Tree Recursion
 
 defmodule Math do
-	
-	def fib1(0), do: 0
-	def fib1(1), do: 1
-	def fib1(n), do: fib1(n-1) + fib1(n-2)
+  
+  def fib1(0), do: 0
+  def fib1(1), do: 1
+  def fib1(n), do: fib1(n-1) + fib1(n-2)
 
-	def fib2(n), do: fib2Iter(1, 0, n)
+  def fib2(n), do: fib2Iter(1, 0, n)
 
-	defp fib2Iter(_, b, 0), do: b
-	defp fib2Iter(a, b, count), do: fib2Iter(a+b, a, count-1)
+  defp fib2Iter(_, b, 0), do: b
+  defp fib2Iter(a, b, count), do: fib2Iter(a+b, a, count-1)
 
 end
+
