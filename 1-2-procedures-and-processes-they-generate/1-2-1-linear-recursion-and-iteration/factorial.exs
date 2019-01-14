1fb04cfbbd1abff1cd999c0abf000a13a4a2467c
Mike <mike42@hotmail.de>
2019-01-14 18:59:46 +0100

    when clause embedded

--- 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.exs
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.exs
@@ -4,11 +4,11 @@
 defmodule Math do
 
   def fact1(1), do: 1
-  def fact1(n), do: n * Math.fact1(n - 1)
+  def fact1(n) when n > 1, do: n * fact1(n - 1)
 
   # Iterative
 
-  def fact2(n), do: fact2_iter(1, 1, n)
+  def fact2(n) when n >= 1, do: fact2_iter(1, 1, n)
   defp fact2_iter(product, counter, max_count) when counter > max_count, do: product
   defp fact2_iter(product, counter, max_count) do
     fact2_iter((counter * product), (counter + 1), max_count)
--- 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.exs
+++ 1-2-procedures-and-processes-they-generate/1-2-2-tree-recursion/fibonacci.exs
@@ -2,12 +2,12 @@
 # Tree Recursion
 
 defmodule Math do
-  
+
   def fib1(0), do: 0
   def fib1(1), do: 1
-  def fib1(n), do: fib1(n-1) + fib1(n-2)
+  def fib1(n) when n > 1, do: fib1(n-1) + fib1(n-2)
 
-  def fib2(n), do: fib2Iter(1, 0, n)
+  def fib2(n) when n >= 0, do: fib2Iter(1, 0, n)
 
   defp fib2Iter(_, b, 0), do: b
   defp fib2Iter(a, b, count), do: fib2Iter(a+b, a, count-1)
