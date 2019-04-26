c7f064e4345a6ef07dbbd7e12bfae89dcffadf87
Mike <mike42@hotmail.de>
2019-04-26 13:09:09 +0200

    Crystal added

---
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.cr
@@ -0,0 +1,21 @@
+# SICP 1.2.1
+# Linear Recursion and Interation
+
+# Crystal version is identical to the Ruby version (no changes neccessary)
+
+def fact1(n)
+  return 1 if n == 1
+  return n * fact1(n - 1)
+end
+
+# Iterative
+
+def fact2(n)
+  return fact2iter(1, 1, n)
+end
+
+def fact2iter(product, counter, maxcount)
+  return product if counter > maxcount
+  return fact2iter(counter*product, counter + 1, maxcount)
+end
+
