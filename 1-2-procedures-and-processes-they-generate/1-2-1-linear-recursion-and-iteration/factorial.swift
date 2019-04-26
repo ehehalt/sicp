d2d6d82a8873a151a785128e0c7a4567ad28b84b
Mike <mike42@hotmail.de>
2019-04-26 13:02:01 +0200

    Swift solution added ...

---
+++ 1-2-procedures-and-processes-they-generate/1-2-1-linear-recursion-and-iteration/factorial.swift
@@ -0,0 +1,22 @@
+// SICP 1.2.1
+// Linear Recursion and Iteration
+
+func fact1(for n: Int) -> Int {
+    guard n != 1 else { return 1 }
+    return n * (fact1(for: (n-1)))
+}
+
+// Iterative
+
+func fact2(for n: Int) -> Int {
+    func fact2Iter(_ product: Int, _ counter: Int, _ maxCount: Int) -> Int {
+        if counter > maxCount {
+            return product
+        } else {
+            return fact2Iter(counter*product, counter+1, maxCount)
+        }
+    }
+    
+    return fact2Iter(1, 1, n)
+}
+
