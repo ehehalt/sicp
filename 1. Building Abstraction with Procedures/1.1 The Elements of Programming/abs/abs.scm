85dfb8c47c5249afe56bade314c0cf50c4b7ce20
ehehalt <mike42@hotmail.de>
2020-07-20 16:01:51 +0200

    abs 1 2 3

--- 1. Building Abstraction with Procedures/1.1 The Elements of Programming/abs/abs.scm
+++ 1. Building Abstraction with Procedures/1.1 The Elements of Programming/abs/abs.scm
@@ -1,6 +1,15 @@
 ;; SICP 1.1.4
 ;; Compound Procedures
 
-(define (abs x)
-  (if (< 0 x) (- x)
+(define (abs1 x)
+  (cond ((< x 0) (- x))
+        ((= x 0) x)
+        ((> x 0) x)))
+
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(define (abs3 x)
+  (if (< x 0) (- x)
       x))
